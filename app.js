03400940c308b2ea4c1d48020dfbf512d1678473
danjake102 <danjakeramos@gmail.com>
2019-03-07 02:23:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,67 @@
-function generateLink() {
-	var id = document.getElementById("surveyID").value;
-	var list = 0
-	var stsLink = "http://survey-d.researchnow.com/survey/selfserve/53b/"+id+"?list=" 
-	var stsLink2 = "&rnid=$rnid&study=$study&c=$c&src=$src"
-	var dkLink = "http://survey-d.researchnow.com/survey/selfserve/53b/"+id+"?list="
-	var dkLink2 = "&pid=$pid&psid=$psid"
-	
+function platformType(){
 	if(document.getElementById("sts").checked == true){
-		list = 1
-		if(document.getElementById("yes").checked == true){
-			document.getElementById("stsLink").innerHTML = ""
-			document.getElementById("stsLinkDigital1").innerHTML = "Exposed: "+ stsLink + list + stsLink2 + "&hType=1"
-			document.getElementById("stsLinkDigital2").innerHTML = "Control: "+ stsLink + list + stsLink2 + "&hType=2"
-		}
-		else{
-			document.getElementById("stsLinkDigital1").innerHTML = ""
-			document.getElementById("stsLinkDigital2").innerHTML = ""
-			document.getElementById("stsLink").innerHTML = "Live link: " + stsLink + list + stsLink2
+		var type = {
+			"type": "sts"
 		}
 	}
-	
 	else{
-		list = 2
-		if(document.getElementById("yes").checked == true){
-			document.getElementById("stsLink").innerHTML = ""
-			document.getElementById("stsLinkDigital1").innerHTML = "Exposed: "+ dkLink + list + dkLink2 + "&hType=1"
-			document.getElementById("stsLinkDigital2").innerHTML = "Control: "+ dkLink + list + dkLink2 + "&hType=2"
+		var type = {
+			"type": "dk"
 		}
-		else{
-			document.getElementById("stsLinkDigital1").innerHTML = ""
-			document.getElementById("stsLinkDigital2").innerHTML = ""
-			document.getElementById("stsLink").innerHTML = "Live link: " + dkLink + list + dkLink2
-		}
+		
 	}
 
+	var livelink = getURL(type)
 
-	
-	
+	setURL(livelink)
+}
 
-	
+
+function getURL(type) {
+	var id = document.getElementById("surveyID").value;
+	var url = "http://survey-d.researchnow.com/survey/selfserve/53b/"+id+"?"
+
+	var platformType = {
+		"sts": url + "list=1&rnid=$rnid&study=$study&c=$c&src=$src",
+		"dk": url + "list=2&pid=$pid&psid=$psid"
+	}
+
+	return platformType[type.type]
+}
+
+
+
+function setURL(livelink){
+
+	var exposed = document.getElementById('liveLinkDigital1')
+	var control = document.getElementById('liveLinkDigital2')
+	var liveLinkElement = document.getElementById('liveLink')
+
+	if(document.getElementById("yes").checked == true){
+		var exposedLink = livelink + "&hType=1"
+		var controlLink = livelink + "&hType=2"
+		exposed.innerHTML = "Exposed: " + exposedLink.link(livelink + "&hType=1")
+		control.innerHTML = "Control: " + controlLink.link(livelink + "&hType=2")
+		clearValues()
+	}
+	else{
+		var live = livelink
+		liveLinkElement.innerHTML = "Live link: " + live.link(livelink)
+		clearValues()
+	}
+}
+
+function clearValues(){
+
+	var exposed = document.getElementById('liveLinkDigital1')
+	var control = document.getElementById('liveLinkDigital2')
+	var liveLinkElement = document.getElementById('liveLink')
+
+	if(document.getElementById("yes").checked == true){
+		liveLinkElement.innerHTML = ""
+	}
+	else{
+		exposed.innerHTML = ""
+		control.innerHTML = ""
+	}
 }
