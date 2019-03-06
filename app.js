6f011e179220df5dd879c2ff90c16005d75710df
danjake102 <danjakeramos@gmail.com>
2019-03-07 05:56:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
+
 function platformType(){
-	if(document.getElementById("sts").checked == true){
-		var type = {
-			"type": "sts"
-		}
+	if(document.getElementById('surveyID').value == ""){
+		alert("input survey ID")
 	}
 	else{
-		var type = {
-			"type": "dk"
-		}
-		
+		if(document.getElementById("sts").checked == true){
+				var type = {
+					"type": "sts"
+				}
+			}
+			else{
+				var type = {
+					"type": "dk"
+				}
+				
+			}
 	}
+	
 
 	var livelink = getURL(type)
 
@@ -36,17 +46,24 @@ function setURL(livelink){
 	var exposed = document.getElementById('liveLinkDigital1')
 	var control = document.getElementById('liveLinkDigital2')
 	var liveLinkElement = document.getElementById('liveLink')
+	var copyToClipboard = document.getElementById('copy')
+	var testing = document.getElementById('copyUrls')
 
 	if(document.getElementById("yes").checked == true){
 		var exposedLink = livelink + "&hType=1"
 		var controlLink = livelink + "&hType=2"
 		exposed.innerHTML = "Exposed: " + exposedLink.link(livelink + "&hType=1")
 		control.innerHTML = "Control: " + controlLink.link(livelink + "&hType=2")
+		testing.value = "Exposed: " + livelink + "&hType=1" + '\n \n' + "Control: " + livelink + "&hType=2"
+		copyToClipboard.innerHTML = '<a class="waves-effect waves-light btn-small" onclick="copyLinks()">Copy to clipboard</a>'
 		clearValues()
 	}
 	else{
 		var live = livelink
 		liveLinkElement.innerHTML = "Live link: " + live.link(livelink)
+		testing.value = livelink
+		copyToClipboard.innerHTML = '<a class="waves-effect waves-light btn-small" onclick="copyLinks()">Copy to clipboard</a>'
+		
 		clearValues()
 	}
 }
@@ -57,6 +74,7 @@ function clearValues(){
 	var control = document.getElementById('liveLinkDigital2')
 	var liveLinkElement = document.getElementById('liveLink')
 
+
 	if(document.getElementById("yes").checked == true){
 		liveLinkElement.innerHTML = ""
 	}
@@ -64,4 +82,12 @@ function clearValues(){
 		exposed.innerHTML = ""
 		control.innerHTML = ""
 	}
+}
+
+function copyLinks(){
+	var copyText = document.getElementById("copyUrls");
+
+	copyText.select();
+
+	document.execCommand("copy");
 }
